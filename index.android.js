33d3d0eaa47a954ce34a68c5e8b9a3cdac782f8b
isma91 <ismaydogmus@gmail.com>
2016-08-16 21:54:12 +0200

    remove unusued style

--- index.android.js
+++ index.android.js
@@ -36,16 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('Pokealert', () => Pokealert);
