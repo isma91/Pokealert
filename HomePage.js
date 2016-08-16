5faad56fde45b77e4da08bc83444ba59625abc6c
isma91 <ismaydogmus@gmail.com>
2016-08-16 21:54:55 +0200

    Adding button and text to navigate trought page

--- HomePage.js
+++ HomePage.js
@@ -18,6 +18,11 @@ class HomePage extends Component {
             username: ""
         };
     }
+    navigate (pageName) {
+        this.props.navigator.push({
+            pageName: pageName
+        })
+    }
 
     render() {
         return (
@@ -25,6 +30,42 @@ class HomePage extends Component {
                 <Text style={styles.title}>
                     Welcome to PokeAlert !!
                 </Text>
+                <Text style={styles.text}>
+                    To add a Pokemon, click to the "Contribution" button !!
+                </Text>
+                <Button
+                    onPress={this.navigate.bind(this, "ContributionPage")}
+                    containerStyle={styles.buttonContainer}
+                    style={styles.button}>
+                    Contribution
+                </Button>
+                <Text style={styles.text}>
+                    To add see all Pokemon in your area, click to the "Consultation" button !!
+                </Text>
+                <Button
+                    onPress={this.navigate.bind(this, "ConsultationPage")}
+                    containerStyle={styles.buttonContainer}
+                    style={styles.button}>
+                    Consultation
+                </Button>
+                <Text style={styles.text}>
+                    To sign up, click to the "Inscription" button !!
+                </Text>
+                <Button
+                    onPress={this.navigate.bind(this, "InscriptionPage")}
+                    containerStyle={styles.buttonContainer}
+                    style={styles.button}>
+                    Inscription
+                </Button>
+                <Text style={styles.text}>
+                    To login, click to the "Connexion" button !!
+                </Text>
+                <Button
+                    onPress={this.navigate.bind(this, "ConnexionPage")}
+                    containerStyle={styles.buttonContainer}
+                    style={styles.button}>
+                    Connexion
+                </Button>
             </View>
         );
     }
@@ -35,19 +76,29 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F5FCFF',
     },
-    pokemonInput: {
-        color: '#000000',
-    },
     title: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
     },
-    instructions: {
+    text: {
         textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+        fontSize: 15,
+        marginTop: 10,
+        marginBottom: 10,
     },
+    buttonContainer : {
+        marginTop: 10,
+        padding:10,
+        height:45,
+        overflow:'hidden',
+        borderRadius:4,
+        backgroundColor: 'black',
+        marginBottom: 10,
+    },
+    button: {
+        color: "#FFFFFF",
+    }
 });
 
 AppRegistry.registerComponent('Pokealert', () => HomePage);
