5c997bc4602207f1f05639b91ae1347bb6118037
isma91 <ismaydogmus@gmail.com>
2016-08-17 17:33:47 +0200

    adding navigator

--- index.android.js
+++ index.android.js
@@ -9,33 +9,36 @@ import {
 } from 'react-native';
 var HomePage = require("./HomePage");
 var ContributionPage = require('./ContributionPage');
+var ConsultationPage = require('./ConsultationPage');
 
 class Pokealert extends Component {
 
-   renderScene(route, navigation) {
-     if (route.pageName === "HomePage") {
-      return <HomePage navigator={navigation}/>
-    } else if (route.pageName === "ContributionPage") {
-      return <ContributionPage navigator={navigation}/>
+    renderScene(route, navigation) {
+        if (route.pageName === "HomePage") {
+            return <HomePage navigator={navigation}/>
+        } else if (route.pageName === "ContributionPage") {
+            return <ContributionPage navigator={navigation}/>
+        } else if (route.pageName === "ConsultationPage") {
+            return <ConsultationPage navigator={navigation}/>
+        }
     }
-  }
 
-  render() {
-    return (
-        <View style={styles.container}>
-          <Navigator
-              initialRoute={{pageName: "HomePage"}}
-              renderScene={this.renderScene.bind(this)}
-          />
-        </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Navigator
+                    initialRoute={{pageName: "HomePage"}}
+                    renderScene={this.renderScene.bind(this)}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 AppRegistry.registerComponent('Pokealert', () => Pokealert);
