64109c7888688404efcfd1cb67fe9310fd19a7e6
isma91 <ismaydogmus@gmail.com>
2016-08-19 13:49:12 +0200

    adding the sample of the config file

---
+++ pokealert_api/config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Config-sample.php
+*
+* This is the config file, you need to change the host, dbname, user, password
+* to use this project and make it work !!
+* Don't forget to rename this file to config.php !!
+*
+* PHP 7.0.8
+*
+* @category Model
+* @package  Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+return [
+	'host' => 'localhost',
+	'dbname' => 'pokealert',
+	'user' => 'root',
+	'password' => '******'
+];
