4d3f59409921cbabe5fe9169f755abeddfbca402
isma91 <ismaydogmus@gmail.com>
2016-08-22 11:23:48 +0200

    fix that it's a model and not a controller

--- pokealert_api/model/User.php
+++ pokealert_api/model/User.php
@@ -6,8 +6,8 @@
 *
 * PHP 7.0.8
 *
-* @category Controller
-* @package  Controller
+* @category Model
+* @package  Model
 * @author   isma91 <ismaydogmus@gmail.com>
 * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
 */
