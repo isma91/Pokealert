7379c852f992567258727e7503e8ac0d888691b2
isma91 <ismaydogmus@gmail.com>
2016-08-23 20:00:52 +0200

    fix typo and more check for the create user

--- pokealert_api/controller/UsersController.php
+++ pokealert_api/controller/UsersController.php
@@ -1,177 +1,211 @@
 <?php
 /**
-* UsersController.php
-*
-* A controller to check the user
-*
-* PHP 7.0.8
-*
-* @category Controller
-* @package  Controller
-* @author   isma91 <ismaydogmus@gmail.com>
-* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
-*/
+ * UsersController.php
+ *
+ * A controller to check the user
+ *
+ * PHP 7.0.8
+ *
+ * @category Controller
+ * @package  Controller
+ * @author   isma91 <ismaydogmus@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
 namespace controller;
 
 use model\Bdd;
 use model\User;
 class UsersController extends User
 {
-    public function send_json($error, $data)
+    public function sendJson($error, $data)
     {
         echo json_encode(array("error" => $error, "data" => $data));
     }
 
-    private function _check_token ($token)
+    private function _checkToken ($id, $token)
     {
         $bdd = new Bdd();
-        $get_token = $bdd->getBdd()->prepare("SELECT token FROM users WHERE id = :id");
-        $get_token->bindParam(":id", $_SESSION["id"]);
-        $get_token->execute();
-        $bdd_token = $get_token->fetch(\PDO::FETCH_ASSOC);
-        if ($token === $bdd_token["token"]) {
+        $field = array("token");
+        $where = "id = $id";
+        $bddToken = $bdd->select("user", $field, $where)[0];
+        if ($token === $bddToken["token"]) {
             return true;
         } else {
             return false;
         }
     }
 
-    static public function is_connected ()
+    public function create($lastname, $firstname, $login, $pass, $confirmPass)
     {
+        $errorCreate = "";
         $bdd = new Bdd();
-        if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
-            return false;
+        if (strlen($pass) < 5) {
+            $errorCreate = $errorCreate . "Password must be at lest 5 characters !!\n";
         }
-        $get = $bdd->getBdd()->prepare('SELECT id, token, login FROM users WHERE id = :id AND token = :token');
-        $get->bindParam(':id', $_SESSION['id']);
-        $get->bindParam(':token', $_SESSION['token']);
-        $get->execute();
-        $user = $get->fetch(\PDO::FETCH_ASSOC);
-        if ($user) {
-            return true;
+        if ($pass !== $confirmPass) {
+            $errorCreate = $errorCreate . "Password are not the same !!\n";
         }
- {
-            return false;
+        if (empty($lastname) || empty($login) || empty($pass) || empty($confirmPass)) {
+            $errorCreate = $errorCreate . "One of the filed is empty !!\n";
+        }
+        if ($login === "anonymous") {
+            $errorCreate = $errorCreate . "You can't take this login !!\n";
+        }
+        if ($errorCreate === "") {
+            $hashedPass = $this->_hashPassword($pass);
+            if (!$this->_checkLogin($login)) {
+                self::sendJson('Login already in use', null);
+                return false;
+            }
+            $arrayFieldAndValue = array("lastname" => $lastname, "firstname" => $firstname, "login" => $login, "password" => $hashedPass);
+            $sql = $bdd->insert('user', $arrayFieldAndValue);
+            if ($sql) {
+                self::sendJson(null, null);
+            } else {
+                self::sendJson('A problem occurred while adding your data in the database !! Please contact the admin of the application !!', null);
+            }
+        } else {
+            self::sendJson($errorCreate, null);
         }
     }
 
     public function connexion($login, $password)
     {
         $bdd = new Bdd();
-        $get_user = $bdd->getBdd()->prepare('SELECT id, pass FROM users WHERE login = :login');
-        $get_user->bindParam(':login', $login);
-        $get_user->execute();
-        $user = $get_user->fetch(\PDO::FETCH_ASSOC);
-        $hashed_pass = $user['pass'];
-        if (!$hashed_pass) {
-            self::send_json("Bad login or password", null);
+        $where = "login = '" . $login . "'";
+        $field = array("id", "password");
+        $user = $bdd->select("user", $field, $where)[0];
+        $hashed_pass = $user['password'];
+        if (!$hashed_pass || !$this->_checkPassword($password, $hashed_pass)) {
+            self::sendJson("Bad login or password", null);
             return false;
         }
-        if (!$this->_check_password($password, $hashed_pass)) {
-            self::send_json("Bad login or password", null);
-            return false;
-        }
-        if (!$this->_update_token($user['id'])) {
-            self::send_json("A problem occurred when we create a token for you !! Please contact the admin of the site !!", null);
-        }
- {
-            self::send_json(null, null);
+        if (!$this->_updateToken($user['id'])) {
+            self::sendJson("A problem occurred when we create a token for you !! Please contact the admin of the application !!", null);
+        } else  {
+            $where2 = "login = '" . $login . "'";
+            $field2 = array("id", "login", "token");
+            $user2 = $bdd->select("user", $field2, $where2)[0];
+            self::sendJson(null, $user2);
         }
     }
 
-    public function edit_login($id, $old_login, $new_login, $token)
+    public function getAllUserMarkOnContribution($id, $token)
     {
         $bdd = new Bdd();
-        if (!$this->_check_token($token)) {
-            self::send_json("Bad token !! Logout and login to avoid the problem !!", null);
-            return false;
-        }
-        $get_old_login = $bdd->getBdd()->prepare("SELECT login FROM users WHERE id = :id");
-        $get_old_login->bindParam(":id", $id);
-        $get_old_login->execute();
-        $user_old_login = $get_old_login->fetch(\PDO::FETCH_ASSOC);
-        if ($user_old_login["login"] !== $old_login) {
-            self::send_json("Bad old login !!", null);
-            return false;
-        }
-        $update_login = $bdd->getBdd()->prepare('UPDATE users SET login = :login WHERE id = :id');
-        $update_login->bindParam(":login", $new_login);
-        $update_login->bindParam(":id", $id);
-        if (!$update_login->execute()) {
-            self::send_json("A problem occurred when we update your login !! Please contact the admin of the site !!", null);
-        }
- {
-            self::send_json(null, null);
+        if(!$this->_checkToken($id, $token)) {
+            self::sendJson("Bad token !! Logout and login to avoid the problem !!", null);
+        } else {
+            $where = "id = $id";
+            $field = array("contributionId");
+            $userMarkOnContribution = $bdd->select("user", $field, $where);
+            self::sendJson(null, $userMarkOnContribution);
         }
     }
 
-    public function edit_password($id, $old_password, $new_password, $token)
+    /*public function editLogin($id, $oldLogin, $newLogin, $token)
     {
         $bdd = new Bdd();
-        if (!$this->_check_token($token)) {
-            self::send_json("Bad token !! Logout and login to avoid the problem !!", null);
+        if (!$this->_checkToken($token)) {
+            self::sendJson("Bad token !! Logout and login to avoid the problem !!", null);
             return false;
         }
-        $get_old_password = $bdd->getBdd()->prepare("SELECT pass FROM users WHERE id = :id");
-        $get_old_password->bindParam(":id", $id);
-        $get_old_password->execute();
-        $user_old_password = $get_old_password->fetch(\PDO::FETCH_ASSOC);
-        if (!$this->_check_password($old_password, $user_old_password["pass"])) {
-            self::send_json("Bad old password !!", null);
+        $getOldLogin = $bdd->getBdd()->prepare("SELECT login FROM users WHERE id = :id");
+        $getOldLogin->bindParam(":id", $id);
+        $getOldLogin->execute();
+        $userOldLogin = $getOldLogin->fetch(\PDO::FETCH_ASSOC);
+        if ($userOldLogin["login"] !== $old_login) {
+            self::sendJson("Bad old login !!", null);
             return false;
         }
-        $new_password = $this->_hash_password($new_password);
-        $update_password = $bdd->getBdd()->prepare('UPDATE users SET pass = :pass WHERE id = :id');
-        $update_password->bindParam(":pass", $new_password);
-        $update_password->bindParam(":id", $id);
-        if (!$update_password->execute()) {
-            self::send_json("A problem occurred when we update your password !! Please contact the admin of the site !!", null);
-        }
- {
-            self::send_json(null, null);
+        $updateLogin = $bdd->getBdd()->prepare('UPDATE users SET login = :login WHERE id = :id');
+        $updateLogin->bindParam(":login", $new_login);
+        $updateLogin->bindParam(":id", $id);
+        if (!$updateLogin->execute()) {
+            self::sendJson("A problem occurred when we update your login !! Please contact the admin of the application !!", null);
+        } else {
+            self::sendJson(null, null);
         }
     }
 
-    private function _update_token($id)
+    public function editPassword($id, $oldPassword, $newPassword, $token)
+    {
+        $bdd = new Bdd();
+        if (!$this->_checkToken($token)) {
+            self::sendJson("Bad token !! Logout and login to avoid the problem !!", null);
+            return false;
+        }
+        $getOldPassword = $bdd->getBdd()->prepare("SELECT pass FROM users WHERE id = :id");
+        $getOldPassword->bindParam(":id", $id);
+        $getOldPassword->execute();
+        $user_old_password = $getOldPassword->fetch(\PDO::FETCH_ASSOC);
+        if (!$this->_checkPassword($old_password, $user_old_password["pass"])) {
+            self::sendJson("Bad old password !!", null);
+            return false;
+        }
+        $newPassword = $this->_hashPassword($newPassword);
+        $updatePassword = $bdd->getBdd()->prepare('UPDATE users SET pass = :pass WHERE id = :id');
+        $updatePassword->bindParam(":pass", $newPassword);
+        $updatePassword->bindParam(":id", $id);
+        if (!$updatePassword->execute()) {
+            self::sendJson("A problem occurred when we update your password !! Please contact the admin of the application !!", null);
+        } else {
+            self::sendJson(null, null);
+        }
+    }*/
+
+    private function _updateToken($id)
     {
         $bdd = new Bdd();
         $token = sha1(time() * rand(1, 555));
-        $update_token = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE id = :id');
-        $update_token->bindParam(':token', $token, \PDO::PARAM_STR, 60);
-        $update_token->bindParam(':id', $id);
-        if ($update_token->execute()) {
-            $_SESSION['token'] = $token;
-            $_SESSION['id'] = $id;
+        $field = array("token" => $token);
+        $where = "id = $id";
+        $updateToken = $bdd->update('user', $field, $where);
+        if ($updateToken) {
             return true;
-        }
- {
+        } else {
             return false;
         }
     }
 
-    private function _hash_password($password)
+    private function _hashPassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
-    private function _check_password($password, $hash)
+    private function _checkPassword($password, $hash)
     {
         return password_verify($password, $hash);
     }
 
-    public function logout($token)
+    private function _checkLogin($login)
     {
         $bdd = new Bdd();
-        $get_token = $bdd->getBdd()->prepare('SELECT token FROM users WHERE id = :id');
-        $get_token->bindParam(':id', $_SESSION['id']);
-        $get_token->execute();
-        $user_token = $get_token->fetch(\PDO::FETCH_ASSOC);
-        if ($user_token["token"] === $token) {
-            session_destroy();
-            self::send_json(null, null);
+        $field = array("login");
+        $where = "login = '" . "$login" . "'";
+        $user = $bdd->select('user', $field, $where);
+        if (empty($user)) {
+            return true;
+        } else {
+            return false;
         }
- {
-            self::send_json("Bad token !! Please delete your cache and your cookie of this site !!", null);
+    }
+
+    public function logout($id, $token)
+    {
+        $bdd = new Bdd();
+        $field = array("token");
+        $where = "id = $id";
+        $userToken = $bdd->select("user", $field, $where);
+        /*$getToken = $bdd->getBdd()->prepare('SELECT token FROM users WHERE id = :id');
+        $getToken->bindParam(':id', $id);
+        $getToken->execute();
+        $userToken = $getToken->fetch(\PDO::FETCH_ASSOC);*/
+        if ($userToken["token"] === $token) {
+            //set the token to null
+            self::sendJson(null, null);
+        } else {
+            self::sendJson("Bad token !! Please delete the cache of the application !!", null);
         }
     }
 
